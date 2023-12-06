85188eb368b0b6fa5b1647fb903dbe38786b4359
HazeFury <mab.dev69@gmail.com>
2023-12-06 17:36:12 +0100

    first route to get the list of bike

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,16 +8,20 @@ const router = express.Router();
 
 // Import itemControllers module for handling item-related operations
 const itemControllers = require("./controllers/itemControllers");
+const bikeControllers = require("./controllers/bikeControllers");
 
-// Route to get a list of items
+// // Route to get a list of items
 router.get("/items", itemControllers.browse);
 
-// Route to get a specific item by ID
+// // Route to get a specific item by ID
 router.get("/items/:id", itemControllers.read);
 
-// Route to add a new item
+// // Route to add a new item
 router.post("/items", itemControllers.add);
 
 /* ************************************************************************* */
 
+router.get("/bikes", bikeControllers.getBikes);
+
+/* ************************************************************************* */
 module.exports = router;
--- frontend/src/components/AllMotorcycles/AllMotorcycles.jsx
+++ frontend/src/components/AllMotorcycles/AllMotorcycles.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import BikeCard from "../Home/BikeCard";
 import styles from "./AllMotorcycles.module.css";
-import Data from "../../DataBase/db.json";
 
 export default function AllMotorcycles() {
-  const bikes = Data;
+  const [bikes, setBikes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3310/api/bikes")
+      .then((res) => {
+        setBikes(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3310/api/bikes")
+  //     .then((res) => res.json())
+  //     .then((data) => setBikes(data))
+  //     .catch((error) => console.error(error));
+  // }, []);
 
   return (
     <section className={styles.preview_bike_component}>
